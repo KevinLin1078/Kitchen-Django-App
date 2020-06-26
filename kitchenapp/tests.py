df88a3e6aefaf190ce924da919c9ccacf528ad0a
KevinLin1078 <39530992+KevinLin1078@users.noreply.github.com>
2020-06-26 12:16:36 -0400

    Update tests.py

--- kitchenapp/tests.py
+++ kitchenapp/tests.py
@@ -1,22 +1,13 @@
-# from django.test import TestCase
-import os
-# Create your tests here.
-print(os.environ.get('AWS_ACCESS_KEY'))
+from django.test import TestCase
+from .models import Kitchen
 
 
-import boto3
 
-# s3 = boto3.resource('s3', aws_access_key_id=os.environ.get('AWS_KEY'), aws_secret_access_key=os.environ.get('AWS_ACCESS_KEY'))
-# for bucket in s3.buckets.all():
-#       print(bucket.all)
+class KitchenTestCase(TestCase):
+    def setUp(self):
+        pass
 
-################################################
-# bucket_name = os.environ.get('BUCKET')
-# key = "upload-file"
-
-# s3 = boto3.resource('s3', aws_access_key_id=os.environ.get('AWS_KEY'), aws_secret_access_key=os.environ.get('AWS_ACCESS_KEY'))
-# bucket = s3.Bucket(bucket_name)
-# bucket.upload_file("upload.txt", key)
-# location = boto3.client('s3', aws_access_key_id=os.environ.get('AWS_KEY'), aws_secret_access_key=os.environ.get('AWS_ACCESS_KEY')).get_bucket_location(Bucket=bucket_name)
-# # url = "https://s3-{0}.amazonaws.com/{1}/{2}".format(location, bucket_name, key)
-# print(location)
+    def test_animals_can_speak(self):
+        kitchen = Kitchen.objects.get(id=2)
+        self.assertEqual(kitchen.kitchen_name, 'Pizza')
+        
